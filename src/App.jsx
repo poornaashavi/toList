b813d27e7109cedec402008aff360ccb62d23e09
Poornaash <118628163+poornaashavi@users.noreply.github.com>
2025-01-05 01:19:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './components/Todo';
+const App = () => {
+  return (
+    <div className='bg-stone-900 py-4 min-h-screen '>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
